d4abe7bfb3d2f2f27af0d5f53dbdcd848ae0fe3f
idc-what-u-think <ahmedayomide1000@gmail.com>
2025-09-10 08:02:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-PREFIX=.
-SESSION_ID=./session/firekid-xmd-session
-PORT=3000
-STICKER_PACK_NAME=Firekid XMD
-STICKER_AUTHOR_NAME=Firekid
+module.exports = {
+    PREFIX: '.',
+    SESSION_ID: './session/firekid-xmd-session',
+    PORT: 3000,
+    STICKER_PACK_NAME: 'Firekid XMD',
+    STICKER_AUTHOR_NAME: 'Firekid'
+};
